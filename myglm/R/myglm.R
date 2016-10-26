0e66760b34df819fbdac59aaf64e25840922b413
Haakon Veraas <haakongv@hotmail.com>
2016-10-26 22:45:23 +0200

    add

--- myglm/R/myglm.R
+++ myglm/R/myglm.R
@@ -34,15 +34,14 @@ myglm = function(formula, data = list(), family, ...){
   else if(family == "poisson"){
     offset = model.offset(mf)
     loglik = function(offset, beta, X, y) {
-      if(is.null(X)){
+      if(is.null(X) == 1){
         return (sum(dpois(y, lambda = exp(offset + beta),  log=TRUE)))
+      }else if(is.null(offset) == 1){
+        print("test")
+        return(sum(dpois(y,lambda = exp(X%*%beta), log = TRUE)))
+      }else{
+        return (sum(dpois(y, lambda = exp(offset + X%*%beta),  log=TRUE)))
       }
-      else{
-      return (sum(dpois(y, lambda = exp(offset + X%*%beta),  log=TRUE)))
-      }
-    }
-    loglik1 = function(offset, beta, X, y) {
-      return (sum(dpois(y, lambda = exp(offset + beta),  log=TRUE)))
     }
     beta = optim(par = numeric(dim(t(X))[1]), fn = loglik, method="BFGS", hessian = TRUE, control = list(fnscale=-1), X = X, offset = offset, y = y)
     betanull = optim(par = numeric(1), fn = loglik, method="BFGS", hessian = TRUE, control = list(fnscale=-1), X = NULL, offset = offset, y = y)
@@ -62,12 +61,13 @@ myglm = function(formula, data = list(), family, ...){
     lp = sum(y * log(exp(offset + X %*% beta$par)) - exp(offset + X %*% beta$par) - logsumy)
     null_deviance = 2*(ls - ln)
     res_deviance = 2*(ls-lp)
-    res_df = length(dim(X)[1]) - length(beta$par)
+    res_df = dim(X)[1] - length(beta$par)
     AIC = 2*length(beta$par) - 2*lp
 
     print(AIC)
     print(null_deviance)
     print(res_deviance)
+    print(res_df)
 
     est = list(terms = terms, y = y, x = X, model = mf, offset = offset,
                coefficients = matrix(c(attr(X,"dimnames")[[2]], beta$par), ncol = length(beta$par),nrow = 2, byrow=TRUE),
